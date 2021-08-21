aa4bba0eb00036ef58786a11eac9b6518e8c9919
Varun Sreepathy <varun.sreepathy@gmail.com>
2021-08-20 23:29:09 -0700

    Added in logic to send tweet everytime script runs

--- post_tweets.py
+++ post_tweets.py
@@ -1,4 +1,17 @@
+import tweepy
+
 def run(generated_tweets):
+	api = set_up()
 	for tweet in generated_tweets:
-		print('Need to post: '+tweet)
+		api.update_status(tweet)
 		# post to Twitter
+
+def set_up():
+	consumer_key = 'XXXX'
+	consumer_secret_key = 'XXXX'
+	access_token = 'XXXX'
+	access_token_secret = 'XXXX'
+	auth = tweepy.OAuthHandler(consumer_key, consumer_secret_key)
+	auth.set_access_token(access_token, access_token_secret)
+	api = tweepy.API(auth)
+	return api
