15219d378e7bc07d2023c31385605d6b7ec6eb0b
sidward35 <mathursiddharth2000@gmail.com>
2021-08-21 09:24:26 -0700

    Set tweet length restrictions

--- post_tweets.py
+++ post_tweets.py
@@ -4,7 +4,9 @@ import config
 def run(generated_tweets):
 	api = set_up()
 	for tweet in generated_tweets:
-		api.update_status(tweet)
+		if len(tweet) >= 15 and len(tweet) <= 280:
+			api.update_status(tweet)
+			break
 
 def set_up():
 	auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret_key)
