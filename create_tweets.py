7ebb26eab11d9cc1a81481c1b0715b5582e305f3
sidward35 <mathursiddharth2000@gmail.com>
2021-08-22 17:19:48 -0700

    Add spelling/grammar check

--- create_tweets.py
+++ create_tweets.py
@@ -1,7 +1,13 @@
 import markov
+import language_tool_python
 
 def run(count):
 	markov.parseGen(['', 'parse', 'pol_tweets', '2', 'tweets.txt'])
 	new_tweets = markov.parseGen(['', 'gen', 'pol_tweets', str(count)])
+
+	tool = language_tool_python.LanguageTool('en-US')
+	for i in range(len(new_tweets)):
+		new_tweets[i] = tool.correct(new_tweets[i])
+
 	print(new_tweets)
 	return new_tweets
