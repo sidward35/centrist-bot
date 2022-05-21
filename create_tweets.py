534fe9abccdac97cd2e412f3e0d9d48bc00bd6b8
Siddharth Mathur <mathursiddharth2000@gmail.com>
2022-05-21 04:27:53 -0700

    Update create_tweets.py

--- create_tweets.py
+++ create_tweets.py
@@ -1,14 +1,17 @@
 import markov
 import language_tool_python
 
+def fix_grammar(new_tweets):
+	tool = language_tool_python.LanguageTool('en-US')
+	for i in range(len(new_tweets)):
+		new_tweets[i] = tool.correct(new_tweets[i])
+	return new_tweets
+
 def run_markov(count):
 	markov.parseGen(['', 'parse', 'pol_tweets', '2', 'tweets.txt'])
 	new_tweets = markov.parseGen(['', 'gen', 'pol_tweets', str(count)])
 
-	tool = language_tool_python.LanguageTool('en-US')
-	for i in range(len(new_tweets)):
-		new_tweets[i] = tool.correct(new_tweets[i])
-
+	new_tweets = fix_grammar(new_tweets)
 	print(new_tweets)
 	return new_tweets
 
@@ -27,7 +30,9 @@ def run(count):
 		base_string=random_line('tweets.txt')
 
 	r = requests.post("https://api.deepai.org/api/text-generator", data={'text': base_string}, headers={'api-key': config.deepai_key})
-	new_tweet = r.json().get('output').replace(base_string,'')
+	new_tweet = r.json().get('output').replace(base_string,'').replace('\\xa0','')
+	new_tweet = new_tweet[0:280] if len(new_tweet)>280 else new_tweet
 
+	new_tweet = fix_grammar([new_tweet])
 	print(new_tweet)
-	return [new_tweet[0:280]] if len(new_tweet)>280 else [new_tweet]
+	return [new_tweet]
