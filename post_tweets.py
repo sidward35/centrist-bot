f865ba3133d2bdfda9ac3e77f503c628d5d178c6
Siddharth Mathur <mathursiddharth2000@gmail.com>
2022-05-21 04:21:02 -0700

    Update post_tweets.py

--- post_tweets.py
+++ post_tweets.py
@@ -9,10 +9,6 @@ def run(generated_tweets):
 			break
 
 def set_up():
-	consumer_key = 'uCoW2hmwo7TebEuxUkYTX46x1'
-	consumer_secret_key = '7EesIQ2Gzmdx6K8XmWJQ0pde4Z7bOx9oYPzFj8xhtCzrKyO5Tn'
-	access_token = '1428945741758668801-NZcHXaob4X7vRl8H2lSWuapTH24N5D'
-	access_token_secret = '3f8JEHGuwGPovFhEACXLxnJYBKILGo5tpaifOjsec2n8h'
 	auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret_key)
 	auth.set_access_token(config.access_token, config.access_token_secret)
 	api = tweepy.API(auth)
